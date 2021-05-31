e1b396f8bf587e4b7a5927804057e4c70d4013bd
dkoenig-tutos <84236397+dkoenig-tutos@users.noreply.github.com>
2021-05-31 11:17:03 +0200

    Update setup-kerberos-client.sh

--- scripts/setup-kerberos-client.sh
+++ scripts/setup-kerberos-client.sh
@@ -3,29 +3,20 @@
 set -ex
 
 echo '##########################################################################'
-echo '##### About to run setup-kerberos-client.sh script #############'
+echo '##### Script de configuration d'un client Kerberos : setup-kerberos-client.sh'
 echo '##########################################################################'
-
 
 yum install -y krb5-workstation pam_krb5
 
 cp /etc/krb5.conf /etc/krb5.conf-orig
-# the following replaces a whole line based on a partial match
-# https://stackoverflow.com/questions/11245144/replace-whole-line-containing-a-string-using-sed
+
 sed -i '/default_realm/c\ default_realm = DKOENIG.INFRA' /etc/krb5.conf
-
-
 sed -i 's/# EXAMPLE.COM/  DKOENIG.INFRA/g' /etc/krb5.conf
-
 sed -i 's/#  kdc = kerberos.example.com/   kdc = kdc.dkoenig.infra/g' /etc/krb5.conf
 sed -i 's/#  admin_server = kerberos.example.com/   admin_server = kdc.dkoenig.infra/g' /etc/krb5.conf
-sed -i 's/# }/}/g' /etc/krb5.conf     # this should edit the line that's right after the admin_server line. 
-
+sed -i 's/# }/}/g' /etc/krb5.conf
 sed -i 's/# .example.com = EXAMPLE.COM/ .dkoenig.infra = DKOENIG.INFRA/g' /etc/krb5.conf
 sed -i 's/# example.com = EXAMPLE.COM/ dkoenig.infra = DKOENIG.INFRA/g' /etc/krb5.conf
-
-
-
 
 clienthostname=$(hostname -f)
 kadmin <<EOF
@@ -34,9 +25,5 @@ addprinc -randkey host/$clienthostname
 ktadd host/$clienthostname
 quit
 EOF
-# alternatively could have done:
-# kadmin -p root/admin -w MotDePasse
-
-
 
 exit 0
