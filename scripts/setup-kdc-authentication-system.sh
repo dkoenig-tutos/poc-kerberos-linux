934c60c2ffa19f631c968b4d46fd83652e3d6dba
dkoenig-tutos <84236397+dkoenig-tutos@users.noreply.github.com>
2021-05-31 10:26:49 +0200

    Update setup-kdc-authentication-system.sh

--- scripts/setup-kdc-authentication-system.sh
+++ scripts/setup-kdc-authentication-system.sh
@@ -3,12 +3,10 @@
 set -ex
 
 echo '##########################################################################'
-echo '##### About to run setup-kdc-authentication-system.sh script #############'
+echo '##### Script de configuration du système d'authentification Kerberos : setup-kdc-authentication-system.sh'
 echo '##########################################################################'
 
-
 yum install -y krb5-server krb5-workstation pam_krb5
-
 
 cp /var/kerberos/krb5kdc/kdc.conf /var/kerberos/krb5kdc/kdc.conf-orig
 
@@ -17,31 +15,21 @@ cp /var/kerberos/krb5kdc/kdc.conf /var/kerberos/krb5kdc/kdc.conf-orig
 # firewall-cmd --add-service=kadmin --permanent
 # systemctl restart firewalld.service
 # systemctl enable firewalld.service
-
 # firewall-cmd --list-all
-
 
 sed -i s/EXAMPLE.COM/DKOENIG.INFRA/g /var/kerberos/krb5kdc/kdc.conf
 
-
 # make it only kerberos5 compatible and not backward compatible. 
 sed -i s/#master_key_type/master_key_type/g /var/kerberos/krb5kdc/kdc.conf
-sed -i '/master_key_type/a \ \ default_principal_flags = +preauth' /var/kerberos/krb5kdc/kdc.conf # this inserts a line after a match
-                                                                                                  # https://stackoverflow.com/questions/15559359/insert-line-after-first-match-using-sed
-
+sed -i '/master_key_type/a \ \ default_principal_flags = +preauth' /var/kerberos/krb5kdc/kdc.conf
 
 cp /etc/krb5.conf /etc/krb5.conf-orig
-# the following replaces a whole line based on a partial match
-# https://stackoverflow.com/questions/11245144/replace-whole-line-containing-a-string-using-sed
+
 sed -i '/default_realm/c\ default_realm = DKOENIG.INFRA' /etc/krb5.conf
-
-
 sed -i 's/# EXAMPLE.COM/  DKOENIG.INFRA/g' /etc/krb5.conf
-
 sed -i 's/#  kdc = kerberos.example.com/   kdc = kdc.dkoenig.infra/g' /etc/krb5.conf
 sed -i 's/#  admin_server = kerberos.example.com/   admin_server = kdc.dkoenig.infra/g' /etc/krb5.conf
-sed -i 's/# }/}/g' /etc/krb5.conf     # this should edit the line that's right after the admin_server line. 
-
+sed -i 's/# }/}/g' /etc/krb5.conf
 sed -i 's/# .example.com = EXAMPLE.COM/ .dkoenig.infra = DKOENIG.INFRA/g' /etc/krb5.conf
 sed -i 's/# example.com = EXAMPLE.COM/ dkoenig.infra = DKOENIG.INFRA/g' /etc/krb5.conf
 
@@ -49,7 +37,6 @@ cp /var/kerberos/krb5kdc/kadm5.acl /var/kerberos/krb5kdc/kadm5.acl-orig
 
 sed -i 's/EXAMPLE.COM/DKOENIG.INFRA/g' /var/kerberos/krb5kdc/kadm5.acl
 
-# this is the create the kerberos database. It can take several minutes to finish. 
 kdb5_util create -s -r DKOENIG.INFRA << EOF
 MotDePasseSecret
 MotDePasseSecret
@@ -61,7 +48,6 @@ systemctl enable kadmin
 systemctl start krb5kdc
 systemctl start kadmin
 
-
 kadmin.local <<EOF
 addprinc root/admin
 MotDePasse
@@ -71,9 +57,7 @@ ktadd host/kdc.dkoenig.infra
 quit
 EOF
 
-
-# the above ktadd command ends up creating the following file:
-
+# La commande ktadd ci-dessus doit créer le fichier suivant :
 file /etc/krb5.keytab
 
 exit 0
