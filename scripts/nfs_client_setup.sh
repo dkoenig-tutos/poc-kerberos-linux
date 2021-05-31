5202c492bf4e8ebbe374e635b81dbc20560f49b8
dkoenig-tutos <84236397+dkoenig-tutos@users.noreply.github.com>
2021-05-31 10:12:06 +0200

    Update nfs_client_setup.sh

--- scripts/nfs_client_setup.sh
+++ scripts/nfs_client_setup.sh
@@ -3,7 +3,7 @@
 set -ex
 
 echo '##########################################################################'
-echo '############### About to run nfs_client_setup.sh script ##################'
+echo '############### Script de configuration du client NFS : nfs_client_setup.sh'
 echo '##########################################################################'
 
 yum install -y nfs-utils
@@ -18,28 +18,11 @@ ktadd nfs/$clienthostname
 quit
 EOF
 
-### at this point I think if you run kadmin again, you get the following error message: 
-# [root@nfs-client rw]# kadmin
-# Authenticating as principal host/admin@DKOENIG.INFRA with password.
-# kadmin: Client 'host/admin@DKOENIG.INFRA' not found in Kerberos database while initializing kadmin interface
-### not sure why this happens. but you can override this by running the following instead. 
-# [root@nfs-client rw]# kadmin -p root/admin
-
-# another options is to run:
-# $ kdestroy
-
 systemctl enable nfs-client.target
 systemctl stop nfs-client.target
 systemctl start nfs-client.target
 
-# showmount -e nfs-storage.dkoenig.infra
-# mount -t nfs nfs-storage.dkoenig.infra:/nfs/export_rw /mnt/backups
-
-# kerberos-authentication entry:
 echo 'nfs-storage.dkoenig.infra:/nfs/export_rw   /mnt/backups    nfs    soft,timeo=100,_netdev,rw,sec=krb5   0   0' >> /etc/fstab
 mount -a
-
-
-
 
 exit 0
