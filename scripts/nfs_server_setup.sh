92256f4c16fa79ae45551e15d4705cc5653ee4cc
dkoenig-tutos <84236397+dkoenig-tutos@users.noreply.github.com>
2021-05-31 10:20:19 +0200

    Update nfs_server_setup.sh

--- scripts/nfs_server_setup.sh
+++ scripts/nfs_server_setup.sh
@@ -3,18 +3,14 @@
 set -ex
 
 echo '##########################################################################'
-echo '##### About to run nfs_server_setup.sh script ##################'
+echo '##### Script de configuration serveur NFS : nfs_server_setup.sh'
 echo '##########################################################################'
 
 
-setsebool -P nfs_export_all_rw 1
-setsebool -P nfs_export_all_ro 1
-# Here we made (P)ersistant changes, you can check with 
-getsebool -a | grep nfs_export
+# setsebool -P nfs_export_all_rw 1
+# setsebool -P nfs_export_all_ro 1
+# getsebool -a | grep nfs_export
 
-
-
-# allow nfs traffic through the firewall
 # systemctl start firewalld
 # systemctl enable firewalld
 # firewall-cmd --permanent --add-service=nfs
@@ -22,12 +18,8 @@ getsebool -a | grep nfs_export
 # firewall-cmd --permanent --add-service=rpc-bind
 # systemctl restart firewalld
 
-
-# install nfs server software
 yum install -y nfs-utils
 
-
-# here are the 2 folders to be shared
 #mkdir -p /nfs/export_ro
 mkdir -p /nfs/export_rw
 chown nfsnobody:nobody /nfs/export_rw
@@ -35,7 +27,6 @@ chmod 775 /nfs/export_rw
 
 # semanage fcontext -a -t public_content_rw_t  "/nfs/export_rw(/.*)?"
 # restorecon -Rv /nfs/export_rw
-
 
 clienthostname=$(hostname -f)
 kadmin <<EOF
@@ -45,21 +36,16 @@ ktadd nfs/$clienthostname
 quit
 EOF
 
-
-
-
-# Next we need to add entries to /etc/exports, which is an empty file by default:
-# do 'man exports' to see exmaple confis 
+# Faire 'man exports' pour voir des exemples de configurations 
 #echo '/nfs/export_ro  *(sync)' > /etc/exports
 echo '/nfs/export_rw  *(rw,no_root_squash,sec=krb5)' >> /etc/exports
 
 systemctl start nfs-server
 systemctl enable nfs-server
 
-# You can confirm this has worked by running:
-showmount -e localhost
-# and also:
-cat /var/lib/nfs/etab
-
+# Il est possible de confirmer le bon fonctionnement avec les commandes suivantes :
+# showmount -e localhost
+# et :
+# cat /var/lib/nfs/etab
 
 exit 0
